440fdde452a1042d5bae7007b70699da0ba921f7
SlouchyCheese <yasdf007@yandex.ru>
2019-05-02 18:24:45 +0300

    Published sourcecode

---
+++ antilavrik.playground/Contents.swift
@@ -0,0 +1,17 @@
+//
+//  antilavrik.swift
+//  antilavrik
+//
+//  Created by AloneStranger & Vlaner on 02/05/2019.
+//  Copyright © 2019 AntiLavrikCodingTeam. All rights reserved.
+//
+
+import UIKit
+
+let constanta = "lavrik gay"
+var variabla = "я думаю, что"
+
+public func vladik() {
+    print(variabla + " " + constanta)
+}
+vladik()
